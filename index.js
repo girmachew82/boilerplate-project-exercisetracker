b5df2bf388fa96c59976910eab827fb85cca1ffd
Girmachew Gulint <girmachewabebe@gmail.com>
2024-05-11 08:22:38 +0000

    Post Execrcise

--- index.js
+++ index.js
@@ -28,37 +28,43 @@ const exerciseSchema = new Schema({
   _id: mongoose.Schema.Types.ObjectId,
   user_id: { type: String, required: true },
   description: { type: String, required: true },
-  duration: { type: Number, required: true},
+  duration: { type: Number, required: true },
   date: { type: Date, default: Date.now },
 })
 
 const Exercise = mongoose.model('Exercise', exerciseSchema)
 
-app.post("/api/users", (req, res) => {
-  const userSave = new User({
-    _id: new mongoose.Types.ObjectId(),
-    username: req.body.username
-  })
-  userSave.save()
-  .then((user) => {
-    res.status(201).json({
-      
-        username: userSave.username,
-        _id:userSave._id
-      
+app.post("/api/users", async (req, res) => {
+  const uname = req.body.username
+  const check = await User.find({ username: uname }).exec()
+  if (check.length != 0) {
+    res.json({
+          message: "User already exist",
     })
-  }).catch((error) => {
-    res.json({ "error": "Error trying to create a user" })
-  })
+  } else {
+    const userSave = new User({
+      _id: new mongoose.Types.ObjectId(),
+      username: req.body.username
+    })
+    userSave.save()
+      .then((user) => {
+        res.status(201).json({
+          username: user.username,
+          _id: user._id
+        })
+      }).catch((error) => {
+        res.json({ "error": "Error trying to create a user" })
+      })
+  }
 })
 
 app.get("/api/users", (req, res) => {
   User.find()
-    .then((users) => {
-      return [
-        users.username,
-        users._id
-      ]
+  .select("username _id")
+    .then((results) => {
+      res.json({
+        results
+      })
     })
     .catch((errer) => {
       res.json({ "error": "Error while fetching users" })
@@ -66,30 +72,34 @@ app.get("/api/users", (req, res) => {
 })
 
 app.post("/api/users/:_id/exercises", (req, res) => {
-
-    User.findById(req.params._id)
-     .then((user) => {
+  let strDate = new Date(req.body.date)
+  if (strDate !='Invalid Date')
+    var dateInput = req.body.date
+  else
+  dateInput = Date.now()
+  User.findById(req.params._id)
+    .then((user) => {
       const exerciseSave = new Exercise({
-        _id: new mongoose.Types.ObjectId(), 
-        user_id: req.params._id, 
-        description: req.body.description, 
-        duration: req.body.duration, 
-        date: req.body.date 
+        _id: new mongoose.Types.ObjectId(),
+        user_id: req.params._id,
+        description: req.body.description,
+        duration: req.body.duration,
+        date: dateInput
       })
       exerciseSave.save()
-         res.status(201).json({
-        username:user.username,
+      res.status(201).json({
+        username: user.username,
         description: exerciseSave.description,
         duration: exerciseSave.duration,
-        date: exerciseSave.date,
-        _id:exerciseSave._id
-      
-    })
+        date: exerciseSave.date.toUTCString(),
+        _id: exerciseSave._id
+
+      })
     })
     .catch((error) => {
       error: "Error while creating an exercise"
     })
-    
+
 })
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
